f162489c2a3f0dac7caa222b9f400f8aa42c80c0
huahuasmaster <1443268119@qq.com>
2019-03-31 00:15:23 +0800

    mysql swagger2

--- src/main/java/com/zyz/shopping/controller/UserController.java
+++ src/main/java/com/zyz/shopping/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.zyz.shopping.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("duangduang/api/user")
 public class UserController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
